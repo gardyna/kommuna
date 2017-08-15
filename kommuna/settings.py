e4dc84a78dcfc51e655090db184350baa97e7efc
Dagur <dagur@gangverd.is>
2017-08-15 15:07:18 +0000

    temporary workaround

--- kommuna/settings.py
+++ kommuna/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com", 'localhost', 'kommunan.com']
 
@@ -142,6 +142,6 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = 'smtp.gmail.com' # mail service smtp
     EMAIL_HOST_USER = os.environ.get('EMAIL-USER') # email id
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL-PASSWORD') #password
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD') #password
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
